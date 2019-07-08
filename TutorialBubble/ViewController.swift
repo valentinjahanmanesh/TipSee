256a603177082e18cde99785ebd573b47b825a86
Farshad Jahanmanesh <farshadjahanmanesh@gmail.com>
2019-07-08 13:01:23 +0430

    multiple bubble, show and dismiss

--- TutorialBubble/ViewController.swift
+++ TutorialBubble/ViewController.swift
@@ -16,9 +16,24 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         let hint = HintPointer(with: self)
-        hint.show(for: [(btnToPoint1,"there is some long text"),(btnToPoint2,"hello this is just a test to check if everything is going well or not"),(btnToPoint3,"this button is an idiot"),(btnToPoint4,"hi ^_^")])
+        let m = hint.show(item: (btnToPoint1,"this is some long text"))
+        let z = hint.show(item: (btnToPoint2,"this is some long text"))
+        let y = hint.show(item: (btnToPoint3,"this sdis some long text"))
+        let x = hint.show(item: (btnToPoint4,"this sdis some lon"))
+        
+        
+        Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { (t) in
+            hint.dismiss(item: y)
+            hint.dismiss(item: z)
+            hint.dismiss(item: x)
+            hint.dismiss(item: m)
+        }
     }
 }
 
@@ -32,28 +47,71 @@ class ViewController: UIViewController {
 
 import Foundation
 import UIKit
+
+protocol HintItems: Equatable {
+    var pointTo:UIView {set get}
+    var showView : UIView {set get}
+}
+
 class HintPointer : UIView{
-    typealias ViewsForViews = (pointTo:UIView,showView : UIView)
+    struct HintItem : HintItems {
+        var ID : Int
+        var pointTo: UIView
+        var showView: UIView
+    }
+    
+    /// properties
+    var options : Options = Options.default()
+    private var shadowLayer : CAShapeLayer?
+    private var viewController : UIViewController
     typealias StringsForViews = (pointTo:UIView,showTexts : String)
-    private var views : [ViewsForViews]!
-    func show(for views : [StringsForViews]){
-        self.viewController.view.addSubview(self)
-        self.setHintConsttraints()
-        
-        self.views = views.compactMap({v,text in
-            let label = UILabel()
-            label.text = text
-            label.textAlignment = .center
-            label.lineBreakMode = .byWordWrapping
-            label.numberOfLines = 0
-            label.sizeToFit()
-            label.font = options.font
-            label.textColor = options.foregroundColor
-            return (v, label)
-        })
-        Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { (timer) in
-            self.point(to: self.views[(0...3).randomElement()!].pointTo, with: self.views[(0...3).randomElement()!].showView)
+    private var views = [HintItem]()
+    private var bubbles = [BubbleView]()
+
+    
+    @discardableResult func show(item : StringsForViews)->HintItem{
+        setupBackgroundDim()
+        let viewToShow = HintItem.init(ID: self.views.count, pointTo:item.pointTo,showView:createLabel(for: item.showTexts) as UIView)
+        self.views.append(viewToShow)
+        bubbles.append(self.point(to: viewToShow))
+        createHoleForVisibleViews()
+        return viewToShow
+    }
+    
+    private func setupBackgroundDim(){
+        if views.isEmpty {
+            self.viewController.view.addSubview(self)
+            self.setHintConsttraints()
         }
+    }
+    
+    func dismiss(item : HintItem){
+        if let index  = self.views.lastIndex(where:{$0  == item}) {
+            let bubble = self.bubbles[index]
+            bubble.removeFromSuperview()
+            self.bubbles.remove(at: index)
+            self.views.remove(at: index)
+            createHoleForVisibleViews()
+        }
+        if views.isEmpty {
+            self.removeFromSuperview()
+        }
+    }
+    
+    /// creates a labelView for useing inside a bubble
+    ///
+    /// - Parameter text: label text
+    /// - Returns: generated label view
+    private func createLabel(for text : String)->UILabel{
+        let label = UILabel()
+        label.text = text
+        label.textAlignment = .center
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.sizeToFit()
+        label.font = options.font
+        label.textColor = options.foregroundColor
+        return label
     }
     
     struct Options {
@@ -63,40 +121,47 @@ class HintPointer : UIView{
         let font : UIFont
         let foregroundColor : UIColor
         static func `default`()->HintPointer.Options{
-            return Options.init(bubbleBackgroundColor: .red, backgroundColor: .black,bubblePosition: .bottom,font: UIFont.boldSystemFont(ofSize: 15),foregroundColor: UIColor.white)
+            return Options.init(bubbleBackgroundColor: .red, backgroundColor: .black,bubblePosition: .left,font: UIFont.boldSystemFont(ofSize: 15),foregroundColor: UIColor.white)
         }
     }
-    
-    var options : Options = Options.default()
-    private var shadowLayer : CAShapeLayer?
-    private var viewController : UIViewController
+    private func createHoleForVisibleViews(){
+        shadowLayer?.removeFromSuperlayer()
+        let pathBigRect = UIBezierPath(rect: viewController.view.bounds)
+        
+        self.views.forEach { (item) in
+            // cuts a hole inside the layer
+            let cutPosition = CGRect(origin: item.pointTo.superview!.convert(item.pointTo.frame.origin, to: self.viewController.view), size: item.pointTo.bounds.size)
+            
+            pathBigRect.append(UIBezierPath(roundedRect: cutPosition.insetBy(dx: -4, dy: -4),cornerRadius: 5))
+            
+        }
+        pathBigRect.usesEvenOddFillRule = true
+        self.cutHole(for: pathBigRect.cgPath)
+    }
+   
     init(with vc : UIViewController) {
         self.viewController = vc
         super.init(frame: .zero)
-        
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    lazy var bubble: BubbleView = {
+    private func point(to item : HintItem)->BubbleView{
+        let view = item.pointTo
+        let label = item.showView
+        
+        // creates a new empty bubble
         let bubble = BubbleView(frame: .zero)
         bubble.backColor = self.options.bubbleBackgroundColor
         bubble.arrow = .init(position: .init(distance: .mid(offset: 0), edge: .left), size: .init(width: 10, height: 5))
-        return bubble
-    }()
-    
-    
-    private func point(to view: UIView,with label : UIView){
-        bubble.removeFromSuperview()
-        shadowLayer?.removeFromSuperlayer()
         self.addSubview(bubble)
-        let pointTo = self.setBubbleConstraints(to: view)
+        
+        // binds constraints
+        let pointTo = self.setBubbleConstraints(to: view,for: bubble)
         bubble.setContent(view: label,padding: 8)
-        let cutPosition = CGRect(origin: view.superview!.convert(view.frame.origin, to: viewController.view), size: view.bounds.size)
-        self.cutHole(for: cutPosition.insetBy(dx: 0, dy: 0))
-       
+        
         
         self.layoutIfNeeded()
         // align the arrow
@@ -106,28 +171,29 @@ class HintPointer : UIView{
         }else{
             bubble.arrow = .init(position: .init(distance: .mid(offset:0), edge: pointTo), size: .init(width: 10, height: 5))
         }
-        
+        return bubble
     }
     
     
-    
-    private func setBubbleConstraints(to view : UIView)-> UIRectEdge {
-        var position  = options.bubblePosition
-        bubble.translatesAutoresizingMaskIntoConstraints = false
+    private func findBetterSpace(view: UIView, checkEdges : [UIRectEdge] = [.top,.left,.right,.bottom])->UIRectEdge{
         if view.frame.minX < 100, options.bubblePosition == .left {
-            position = .right
+            return  .right
         }
         if view.frame.maxX > viewController.view.bounds.width - 100, options.bubblePosition == .right {
-            position = .left
+            return .left
         }
         if view.frame.minY > viewController.view.bounds.height - 100, options.bubblePosition == .bottom {
-            position = .top
+            return  .top
         }
         if view.frame.maxY > -100, options.bubblePosition == .bottom {
-            position = .bottom
+            return  .bottom
         }
+        return options.bubblePosition
+    }
+    private func setBubbleConstraints(to view : UIView,for bubble : BubbleView)-> UIRectEdge {
+        let position  = findBetterSpace(view: view)
+        bubble.translatesAutoresizingMaskIntoConstraints = false
         var arrowPoint : UIRectEdge = .right
-        
         switch position {
         case .top,.bottom:
             var TopOrBottomConstraints = bubble.topAnchor.constraint(equalTo: view.bottomAnchor,constant: 16)
@@ -175,20 +241,16 @@ class HintPointer : UIView{
         self.bottomAnchor.constraint(equalTo: self.viewController.view.bottomAnchor).isActive = true
         self.leftAnchor.constraint(equalTo: self.viewController.view.leftAnchor).isActive = true
     }
-    private func cutHole(for rect : CGRect){
-        let pathBigRect = UIBezierPath(rect: viewController.view.frame)
-        let pathSmallRect = UIBezierPath(roundedRect: rect,cornerRadius: 5)
-        
-        pathBigRect.append(pathSmallRect)
-        pathBigRect.usesEvenOddFillRule = true
+    
+    private func cutHole(for path : CGPath){
         
         let fillLayer = CAShapeLayer()
-        fillLayer.path = pathBigRect.cgPath
+        fillLayer.path = path
         fillLayer.fillRule = CAShapeLayerFillRule.evenOdd
         fillLayer.fillColor = options.backgroundColor.cgColor
         fillLayer.opacity = 0.7
-        shadowLayer = fillLayer
         self.layer.insertSublayer(fillLayer, at: 0)
+        shadowLayer = fillLayer
     }
 }
 class BubbleView: UIView {
@@ -313,3 +375,25 @@ class BubbleView: UIView {
     }
 }
 
+
+extension HintPointer {
+    func slideShow(for views : [StringsForViews],interval : TimeInterval){
+        //        self.viewController.view.addSubview(self)
+        //        self.setHintConsttraints()
+        //
+        //        self.views = views.compactMap({v,text in
+        //            let label = UILabel()
+        //            label.text = text
+        //            label.textAlignment = .center
+        //            label.lineBreakMode = .byWordWrapping
+        //            label.numberOfLines = 0
+        //            label.sizeToFit()
+        //            label.font = options.font
+        //            label.textColor = options.foregroundColor
+        //            return (v, label)
+        //        })
+        //        Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { (timer) in
+        //            self.point(to: self.views[].pointTo, with: self.views[(0...3).randomElement()!].showView)
+        //        }
+    }
+}
